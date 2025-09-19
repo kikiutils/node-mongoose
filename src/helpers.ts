e94e6aafb6bca454731fb10392d8f62e32a538c8
kiki-kanri <a470666@gmail.com>
2025-09-19 11:23:06 +0800

    refactor!: remove `mongooseDocumentOrObjectIdToDocument`

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,28 +1,5 @@
 import { Types } from 'mongoose';
 
-import type {
-    BaseMongoosePaginateModel,
-    MongooseDocumentOrObjectId,
-    MongooseHydratedDocument,
-} from './types';
-
-export async function mongooseDocumentOrObjectIdToDocument<
-    D extends MongooseHydratedDocument<DocType, InstanceMethodsAndOverrides, QueryHelpers>,
-    DocType,
-    InstanceMethodsAndOverrides,
-    QueryHelpers,
->(
-    documentOrObjectId: MongooseDocumentOrObjectId<D>,
-    model: BaseMongoosePaginateModel<DocType, InstanceMethodsAndOverrides, QueryHelpers>,
-    selectFields?: string[],
-) {
-    if (typeof documentOrObjectId === 'string' || documentOrObjectId instanceof Types.ObjectId) {
-        return (await model.findById(documentOrObjectId).select(selectFields || [])) as D | null;
-    }
-
-    return documentOrObjectId;
-}
-
 export function toObjectIdHexString(id: string | Types.ObjectId) {
     return id instanceof Types.ObjectId ? id.toHexString() : id;
 }
